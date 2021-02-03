800d8513fde8673b5887809572fe501ff42827ae
Daniela <danielarpo450@gmail.com>
2021-02-02 20:11:39 -0500

    validate if is admin

--- src/api/index.js
+++ src/api/index.js
@@ -7,7 +7,8 @@ app.use(bodyParser.json());
 
 const validateAdmin = (req, res, next) => {
   const userAuth = req.headers.authorization;
-  if (userAuth) {
+  console.log(userAuth);
+  if (userAuth === 'admin') {
     return next();
   }
   res.status(403).json({
